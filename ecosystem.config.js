68e423ac11d7888eabec0cd0e8cd2bbe0eec63d5
MyAppButik Dev <developer@myappbutik.ru>
2025-12-25 12:57:59 +0300

    chore: disable bot autorestart until configured

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,6 +36,7 @@ module.exports = {
       script: 'dist/index.js',
       instances: 1,
       exec_mode: 'fork',
+      autorestart: false,  // Disabled until bot is configured
       env: {
         NODE_ENV: 'production'
       },
